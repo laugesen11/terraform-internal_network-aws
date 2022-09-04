8a0b4de47bcefdac4d406319e61ebc1d7834d199
Larry <laugesen11@gmail.com>
2022-09-04 13:47:20 -0500

    Cleaning up NACL module

--- modules/nacl/variables.tf
+++ modules/nacl/variables.tf
@@ -99,60 +99,6 @@ variable "ingress" {
   )
 }
 
-#List of egress rules for NACL
-#variable "egress" {
-#  description = "Sets the egress (outbound) rules for the NACL"
-#  default = null
-#
-#  type = list(
-#    object({  
-#      #Set the protocol to 'all' for all
-#      #Must set from_port and to_port to '0'
-#      protocol        = string
-#      rule_number     = number 
-#      #Determines if this allows access
-#      deny_access     = bool
-#      cidr_block      = string
-#      ipv6_cidr_block = string
-#      #Set to 0 if protocol set to 'all'
-#      from_port       = number
-#      #Set to 0 if protocol set to 'all'
-#      to_port         = number
-#      #Set to null if not setting ICMP protocol
-#      icmp_type       = string
-#      #Set to null if not setting ICMP protocol
-#      icmp_code       = string
-#    })
-#  )
-#}
-#
-##List of ingress rules for NACL
-#variable "ingress" {
-#  description = "Sets the ingress (inbound) rules for the NACL"
-#  default = null
-#
-#  type = list(
-#    object({  
-#      #Set the protocol to 'all' for all
-#      #Must set from_port and to_port to '0'
-#      protocol        = string
-#      rule_number     = number 
-#      #Determines if this allows access
-#      deny_access     = bool
-#      cidr_block      = string
-#      ipv6_cidr_block = string
-#      #Set to 0 if protocol set to 'all'
-#      from_port       = number
-#      #Set to 0 if protocol set to 'all'
-#      to_port         = number
-#      #Set to null if not setting ICMP protocol
-#      icmp_type       = string
-#      #Set to null if not setting ICMP protocol
-#      icmp_code       = string
-#    })
-#  )
-#}
-
 variable "tags" {
   type = map(string)
   default = {}
