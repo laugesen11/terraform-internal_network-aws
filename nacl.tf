9c33c99f8843b61d036a65cea575a6a712300562
Larry <laugesen11@gmail.com>
2022-09-04 13:40:44 -0500

    Updated to better handle NACLs

--- nacl.tf
+++ nacl.tf
@@ -9,7 +9,7 @@
 locals {
   #Get the VPCs we need to create NACLs for
   nacl_vpcs = {
-    for item in var.nacl_egress_rules:
+    for item in var.nacl_setup:
       #Check if we can pull the ID from the VPC module using the VPC name. If not, we assume this is the VPC ID
       item.nacl_name => lookup(module.vpcs,item.vpc,null) != null ? module.vpcs[item.vpc].vpc.id : item.vpc
   }
@@ -17,27 +17,65 @@ locals {
   nacl_subnets = {
     #Gets the subnets we are creating NACLs in. i
     #We only use egress because it is expected you will set an ingress and egress rule for each subnet
-    for item in var.nacl_ingress_rules:
+    for item in var.nacl_setup:
       item.nacl_name => [
         for subnet in item.subnets: 
           #Check if we can pull the VPC ID from the VPC module using the VPC name. If so, we try to pull the subnet from here
           #If not, we assume this is the ID of an existing subnet
-          #item.nacl_vpc_id == null ? module.vpcs[item.nacl_vpc_name].subnets[subnet].id : subnet
           lookup(module.vpcs,item.vpc,null) == null ? subnet : ( lookup(module.vpcs[item.vpc].subnets,subnet,null) != null ? module.vpcs[item.vpc].subnets[subnet].id : subnet )
       ]
   }
   
   #Sets the egress rulese for this NACL
   nacl_egress_rules = {
-    for item in var.nacl_egress_rules:
-      item.nacl_name => item.nacl_rules
+    for item in var.nacl_setup:
+      item.nacl_name => [
+        for rule in item.nacl_rules: 
+          rule if !contains(rule.options,"is_ingress")
+      ]
+ item.nacl_rules
   }
 
   #Sets the ingress rulese for this NACL
   nacl_ingress_rules = {
-    for item in var.nacl_ingress_rules:
-      item.nacl_name => item.nacl_rules
+    for item in var.nacl_setup:
+      item.nacl_name => [
+        for rule in item.nacl_rules: 
+          rule if contains(rule.options,"is_ingress")
+      ]
   }
+
+  #Get the VPCs we need to create NACLs for
+#  nacl_vpcs = {
+#    for item in var.nacl_egress_rules:
+#      #Check if we can pull the ID from the VPC module using the VPC name. If not, we assume this is the VPC ID
+#      item.nacl_name => lookup(module.vpcs,item.vpc,null) != null ? module.vpcs[item.vpc].vpc.id : item.vpc
+#  }
+#
+#  nacl_subnets = {
+#    #Gets the subnets we are creating NACLs in. i
+#    #We only use egress because it is expected you will set an ingress and egress rule for each subnet
+#    for item in var.nacl_ingress_rules:
+#      item.nacl_name => [
+#        for subnet in item.subnets: 
+#          #Check if we can pull the VPC ID from the VPC module using the VPC name. If so, we try to pull the subnet from here
+#          #If not, we assume this is the ID of an existing subnet
+#          #item.nacl_vpc_id == null ? module.vpcs[item.nacl_vpc_name].subnets[subnet].id : subnet
+#          lookup(module.vpcs,item.vpc,null) == null ? subnet : ( lookup(module.vpcs[item.vpc].subnets,subnet,null) != null ? module.vpcs[item.vpc].subnets[subnet].id : subnet )
+#      ]
+#  }
+#  
+#  #Sets the egress rulese for this NACL
+#  nacl_egress_rules = {
+#    for item in var.nacl_egress_rules:
+#      item.nacl_name => item.nacl_rules
+#  }
+#
+#  #Sets the ingress rulese for this NACL
+#  nacl_ingress_rules = {
+#    for item in var.nacl_ingress_rules:
+#      item.nacl_name => item.nacl_rules
+#  }
 
 }
 
--- variables.tf
+++ variables.tf
@@ -63,7 +63,7 @@ variable "vpc_setup" {
   )
 }
 
-variable "nacl_egress_rules" {
+variable "nacl_setup" {
   description = "Determine the Network Access Control List egress (outbound) traffic rules"
   default = null
 
@@ -102,6 +102,7 @@ variable "nacl_egress_rules" {
           #Sets optional settings for rules
           #Valid values include:
           #  - "deny_access" - set this rule to deny access
+          #  - "is_ingress"  - set this rule to be an ingress. Otherwise, this will be an egress rule
           #  - "icmp_type=<string>"   - sets the ICMP type if handling ICMP
           #  - "icmp_code=<string>"   - sets the ICMP code if handling ICMP
           #  - "from_port=<number>"   - sets the minimum port range for custom setup. Set to '0' for all. If not set, set equal to "to_port"
@@ -114,55 +115,106 @@ variable "nacl_egress_rules" {
   )
 }
 
-variable "nacl_ingress_rules" {
-  description = "Determine the Network Access Control List ingress (inbound) traffic rules"
-  default = null
-
-  type = list(
-      object({
-        nacl_name     = string
-
-        #Set to use VPC created in this module. 
-        #Must set this value to use subnet names for nacl_subnets
-        vpc = string
-
-        #Can be set to subnet IDs or names used in this module
-        subnets  = list(string)
-
-        nacl_rules    = list(object({
-          #Set's the traffic type
-          #Set to null or "custom" to set protocol, to_port, and from_port yourself
-          #Valid options include:
-          #  - "all"                - All traffic to the destination (to_port,from_port=0, protocol="all")
-          #  - "http"               - HTTP traffic to the destination (to_port,from_port=80, protocol="tcp")
-          #  - "https"              - HTTPS traffic to the destination (to_port,from_port=443, protocol="tcp")
-          #  - "ssh"                - SSH traffic to the destination (to_port,from_port=22, protocol="tcp")
-          #  - "telnet"             - Telnet traffic to the destination (to_port,from_port=23, protocol="tcp")
-          #  - "smtp"               - SMTP traffic to the destination (to_port,from_port=25, protocol="tcp")
-          #  - "web site response"  - Traffc to ephemeral ports, usually a response to a web page(from_port=1024,to_port=65535,protocol="tcp")
-          traffic_type        = string
-
-          #Sets the priority of the rule
-          rule_number     = number
-
-          #Sets the IPv4 or IPv6 address of the external IP address we are recieving traffic from or directing traffic to
-          #We use pattern matching on this value to determine which it is
-          external_ip_address = string
-
-          #Sets optional settings for rules
-          #Valid values include:
-          #  - "deny_access" - set this rule to deny access
-          #  - "icmp_type=<string>"   - sets the ICMP type if handling ICMP
-          #  - "icmp_code=<string>"   - sets the ICMP code if handling ICMP
-          #  - "from_port=<number>"   - sets the minimum port range for custom setup. Set to '0' for all. If not set, set equal to "to_port"
-          #  - "to_port=<number>"     - sets the max port port range for custom setup. Set to '0' for all. If not set, set equal to "to_port"
-          #  - "protocol=<string>"    - sets the protocol if we want to handle all that ourselves. Can set to 'all' for all protocols 
-          options         = list(string)
-        })
-      )
-    })
-  )
-}
+#variable "nacl_egress_rules" {
+#  description = "Determine the Network Access Control List egress (outbound) traffic rules"
+#  default = null
+#
+#  type = list(
+#      object({
+#        nacl_name     = string
+#
+#        #Set to use VPC created in this module. 
+#        #Must set this value to use subnet names for nacl_subnets
+#        vpc = string
+#
+#        #Can be set to subnet IDs or names used in this module
+#        subnets  = list(string)
+#
+#        nacl_rules    = list(object({
+#          #Set's the traffic type
+#          #Set to null or "custom" to set protocol, to_port, and from_port yourself
+#          #Valid options include:
+#          #  - "all"                - All traffic from the source (to_port,from_port=0, protocol="all")
+#          #  - "http"               - HTTP traffic from the source (to_port,from_port=80, protocol="tcp")
+#          #  - "https"              - HTTPS traffic from the source (to_port,from_port=443, protocol="tcp")
+#          #  - "ssh"                - SSH traffic from the source (to_port,from_port=22, protocol="tcp")
+#          #  - "telnet"             - Telnet traffic from the source (to_port,from_port=23, protocol="tcp")
+#          #  - "smtp"               - SMTP traffic from the source (to_port,from_port=25, protocol="tcp")
+#          #  - "web site response"  - Traffic from ephemeral ports, usually a responce to a web page(from_port=1024,to_port=65535,protocol="tcp")
+#          traffic_type        = string
+#
+#          #Sets the priority of the rule
+#          rule_number     = number
+#
+#          #Sets the IPv4 or IPv6 address of the external IP address we are recieving traffic from or directing traffic to
+#          #We use pattern matching on this value to determine which it is
+#          external_ip_address = string
+#
+# 
+#          #Sets optional settings for rules
+#          #Valid values include:
+#          #  - "deny_access" - set this rule to deny access
+#          #  - "icmp_type=<string>"   - sets the ICMP type if handling ICMP
+#          #  - "icmp_code=<string>"   - sets the ICMP code if handling ICMP
+#          #  - "from_port=<number>"   - sets the minimum port range for custom setup. Set to '0' for all. If not set, set equal to "to_port"
+#          #  - "to_port=<number>"     - sets the max port port range for custom setup. Set to '0' for all. If not set, set equal to "to_port"
+#          #  - "protocol=<string>"    - sets the protocol if we want to handle all that ourselves. Can set to 'all' for all protocols 
+#          options         = list(string)
+#        })
+#      )
+#    })
+#  )
+#}
+#
+#variable "nacl_ingress_rules" {
+#  description = "Determine the Network Access Control List ingress (inbound) traffic rules"
+#  default = null
+#
+#  type = list(
+#      object({
+#        nacl_name     = string
+#
+#        #Set to use VPC created in this module. 
+#        #Must set this value to use subnet names for nacl_subnets
+#        vpc = string
+#
+#        #Can be set to subnet IDs or names used in this module
+#        subnets  = list(string)
+#
+#        nacl_rules    = list(object({
+#          #Set's the traffic type
+#          #Set to null or "custom" to set protocol, to_port, and from_port yourself
+#          #Valid options include:
+#          #  - "all"                - All traffic to the destination (to_port,from_port=0, protocol="all")
+#          #  - "http"               - HTTP traffic to the destination (to_port,from_port=80, protocol="tcp")
+#          #  - "https"              - HTTPS traffic to the destination (to_port,from_port=443, protocol="tcp")
+#          #  - "ssh"                - SSH traffic to the destination (to_port,from_port=22, protocol="tcp")
+#          #  - "telnet"             - Telnet traffic to the destination (to_port,from_port=23, protocol="tcp")
+#          #  - "smtp"               - SMTP traffic to the destination (to_port,from_port=25, protocol="tcp")
+#          #  - "web site response"  - Traffc to ephemeral ports, usually a response to a web page(from_port=1024,to_port=65535,protocol="tcp")
+#          traffic_type        = string
+#
+#          #Sets the priority of the rule
+#          rule_number     = number
+#
+#          #Sets the IPv4 or IPv6 address of the external IP address we are recieving traffic from or directing traffic to
+#          #We use pattern matching on this value to determine which it is
+#          external_ip_address = string
+#
+#          #Sets optional settings for rules
+#          #Valid values include:
+#          #  - "deny_access" - set this rule to deny access
+#          #  - "icmp_type=<string>"   - sets the ICMP type if handling ICMP
+#          #  - "icmp_code=<string>"   - sets the ICMP code if handling ICMP
+#          #  - "from_port=<number>"   - sets the minimum port range for custom setup. Set to '0' for all. If not set, set equal to "to_port"
+#          #  - "to_port=<number>"     - sets the max port port range for custom setup. Set to '0' for all. If not set, set equal to "to_port"
+#          #  - "protocol=<string>"    - sets the protocol if we want to handle all that ourselves. Can set to 'all' for all protocols 
+#          options         = list(string)
+#        })
+#      )
+#    })
+#  )
+#}
 
 #variable "nacl_ingress_rules" {
 #  description = "Determine the Network Access Control List ingress (outbound) traffic rules"
