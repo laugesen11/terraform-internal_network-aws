f6092d6588c70b24170b857338e1c8a926a0c0a2
Larry <laugesen11@gmail.com>
2022-09-12 06:08:34 -0500

    Updated to better handle VPC peering

--- variables.tf
+++ variables.tf
@@ -171,35 +171,6 @@ variable "vpc_peering" {
   )
 }
 
-#variable "vpc_endpoints" {
-#  description = "Sets up VPC endpoint"
-#  default     = null
-#  type = list(
-#    object({
-#      name                = string
-# 
-#      #Specify the AWS service this endpoint is for
-#      service_name        = string
-#
-#      #The VPC this is assigned to
-#      vpc                 = string
-#
-#      #Sets optional values
-#      #Valid values include:
-#      #  - "auto_accept" - Accept the VPC endpoint (the VPC endpoint and service need to be in the same AWS account).
-#      #  - "private_dns_enabled" -  (AWS services and AWS Marketplace partner services only) Whether or not to associate a private hosted zone with the specified VPC. Applicable for endpoints of type Interface. 
-#      #  - "ip_address_type=<ipv4|ipv6|dual stack>" - The IP address type for the endpoint. Valid values are ipv4, dualstack, and ipv6.
-#      #  - "security_groupsi=<list of security groups>" - list of security groups to associate with this endpoint. Applicable for endpoints of type Interface. Can be security group defined here or external security group  
-#      #  - "subnets=<list of subnets>" - list of subnets this endpoint is for. Applicable for endpoints of type GatewayLoadBalancer and Interface. Can be for subnets defined here or externally  
-#      #  - "route_table_ids=<route_table_ids>" - One or more route table IDs. Applicable for endpoints of type Gateway.
-#      #  - "vpc_endpoint_type=<Gateway|Interface|GatewayLoadBalancer>" - The VPC endpoint type. Gateway, GatewayLoadBalancer, or Interface. Defaults to Gateway.
-#      #  - "dns_record_ip_type=<ipv4|dualstack|service-defined|ipv6>" 
-#      options             = list(string)
-#      tags                = map(string)
-#    })
-#  )
-#}
-
 variable "transit_gateways" {
   description = "Defines Transit Gateway setup"
   default     = null
